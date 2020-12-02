c1f94fbd2be5736d67dd526db0f2b3140d598be7
dominikszczuka <szczuka27@gmail.com>
2020-12-02 16:00:16 +0100

    Test husky

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import './header.css'
-
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./header.css";
 
 const Header = () => {
+  const { t } = useTranslation();
 
-    const { t } = useTranslation();
-    
-
-    return(
-        <header className="header">
-           <p className="header__logo">{t('title-app')}</p> 
-        </header>
-    );
+  return (
+    <header className="header">
+      <p className="header__logo">{t("title-app")} </p>
+    </header>
+  );
 };
 
 export default Header;
--- src/Components/ToDoList/ToDoList.tsx
+++ src/Components/ToDoList/ToDoList.tsx
@@ -1,38 +1,32 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
+import ToDoItem from "../ToDoItem/ToDoItem";
 
-import ToDoItem from '../ToDoItem/ToDoItem';
-
-import  { ToDo }  from 'constants/types/ToDo';
+import { ToDo } from "constants/types/ToDo";
 
 const ToDoList = () => {
+  const listItems: ToDo[] = [
+    {
+      id: 0,
+      title: "Task 1",
+      desc: "Lorem ipsum set dolores",
+    },
+    {
+      id: 1,
+      title: "Task 2",
+      desc: "Lorem ipsum set dolores uno dos tres",
+    },
+    {
+      id: 2,
+      title: "Task 3",
+      desc: "Hello World",
+    },
+  ];
 
-    const listItems:ToDo[] = [
-        {
-            id:0,
-            title: 'Task 1',
-            desc: "Lorem ipsum set dolores"
-        },
-        {
-            id:1,
-            title: 'Task 2',
-            desc: "Lorem ipsum set dolores uno dos tres"
-        },
-        {
-            id:2,
-            title: 'Task 3',
-            desc: "Hello World"
-        }
-    ]
+  const renderToDoList = (): ReactNode =>
+    listItems.map((todo) => <ToDoItem key={todo.id} todo={todo} />);
 
-    const renderToDoList = ():ReactNode => listItems.map( todo => <ToDoItem  key={todo.id} todo={todo} />)
-    
-
-    return (
-        <>
-            {renderToDoList()}
-        </>
-    );
-}
+  return <>{renderToDoList()}</>;
+};
 
 export default ToDoList;
