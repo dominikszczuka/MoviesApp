9a77f804a17a16286849006ab95b28bac65d0eb3
dominikszczuka <szczuka27@gmail.com>
2020-12-02 19:47:52 +0100

    Add HistoryToDoScreen

---
+++ src/Screens/HistoryToDoScreen/HistoryToDoScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HistoryToDoScreen = () => {
+  return <p>History</p>;
+};
+
+export default HistoryToDoScreen;
--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
-import Nav from '../../components/Nav/Nav';
-import ToDo from '../../components/ToDo/ToDo';
+import React from "react";
+import Header from "../../components/Header/Header";
+import Nav from "../../components/Nav/Nav";
+import ToDo from "../../components/ToDo/ToDo";
 
 const HomeScreen = () => {
-    return(
-        <>
-        <Header />
-        <Nav />
-        <ToDo />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Nav />
+      <ToDo />
+    </>
+  );
 };
 
 export default HomeScreen;
