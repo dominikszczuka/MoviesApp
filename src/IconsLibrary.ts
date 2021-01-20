62f1253ca91f1c89540e33cc25b3f2133435f8cc
dominikszczuka <szczuka27@gmail.com>
2021-01-20 19:22:58 +0100

    Add new icons to project

--- src/IconsLibrary.ts
+++ src/IconsLibrary.ts
@@ -1,9 +1,30 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
   faCalendarPlus,
+  faCalendarTimes,
   faGraduationCap,
   faCheckCircle,
   faPlusSquare,
+  faCheck,
+  faBriefcase,
+  faHome,
+  faCar,
+  faChild,
+  faRunning,
+  faWindowClose,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faCalendarPlus, faGraduationCap, faCheckCircle, faPlusSquare);
+library.add(
+  faCalendarPlus,
+  faGraduationCap,
+  faCheckCircle,
+  faPlusSquare,
+  faCalendarTimes,
+  faCheck,
+  faBriefcase,
+  faHome,
+  faCar,
+  faChild,
+  faRunning,
+  faWindowClose
+);
--- src/constants/enums/Icons.ts
+++ src/constants/enums/Icons.ts
@@ -4,4 +4,12 @@ export enum Icons {
   faGraduationCap = "graduation-cap",
   faCheckCircle = "check-circle",
   faPlusSquare = "plus-square",
+  faCalendarTimes = "calendar-times",
+  faCheck = "check",
+  faBriefcase = "briefcase",
+  faHome = "home",
+  faCar = "car",
+  faChild = "child",
+  faRunning = "running",
+  faWindowClose = "window-close",
 }
